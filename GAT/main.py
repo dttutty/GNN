006fdb63e22a9e80cad06abeb4c5ddd43537ab83
Lei Zhao <dttutty@gmail.com>
2024-09-24 11:11:35 -0500

    learning GNN

--- GAT/main.py
+++ GAT/main.py
@@ -2,11 +2,6 @@
 import torch
 torchversion = torch.__version__
 
-# Install PyTorch Scatter, PyTorch Sparse, and PyTorch Geometric
-!pip install -q torch-scatter -f https://data.pyg.org/whl/torch-{torchversion}.html
-!pip install -q torch-sparse -f https://data.pyg.org/whl/torch-{torchversion}.html
-!pip install -q git+https://github.com/pyg-team/pytorch_geometric.git
-
 # Numpy for matrices
 import numpy as np
 np.random.seed(0)
@@ -14,4 +9,142 @@ np.random.seed(0)
 # Visualization
 import networkx as nx
 from sklearn.manifold import TSNE
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from gatv2_conv import GATv2Conv
+from torch_geometric.datasets import Planetoid
+
+# Import dataset from PyTorch Geometric
+dataset = Planetoid(root=".", name="CiteSeer")
+
+data = dataset[0]
+
+# Print information about the dataset
+print(f'Dataset: {dataset}')
+print('-------------------')
+print(f'Number of graphs: {len(dataset)}')
+print(f'Number of nodes: {data.x.shape[0]}')
+print(f'Number of features: {dataset.num_features}')
+print(f'Number of classes: {dataset.num_classes}')
+
+# Print information about the graph
+print(f'\nGraph:')
+print('------')
+print(f'Edges are directed: {data.is_directed()}')
+print(f'Graph has isolated nodes: {data.has_isolated_nodes()}')
+print(f'Graph has loops: {data.has_self_loops()}')
+
+from torch_geometric.utils import remove_isolated_nodes
+
+isolated = (remove_isolated_nodes(data['edge_index'])[2] == False).sum(dim=0).item()
+print(f'Number of isolated nodes = {isolated}')
+
+from torch_geometric.utils import to_networkx
+
+# G = to_networkx(data, to_undirected=True)
+# plt.figure(figsize=(18,18))
+# plt.axis('off')
+# nx.draw_networkx(G,
+#                 pos=nx.spring_layout(G, seed=42),
+#                 with_labels=False,
+#                 node_size=50,
+#                 node_color=data.y,
+#                 width=2,
+#                 edge_color="grey"
+#                 )
+# plt.show()
+
+
+from torch_geometric.utils import degree
+from collections import Counter
+
+# Get list of degrees for each node
+degrees = degree(data.edge_index[0]).numpy()
+
+# Count the number of nodes for each degree
+numbers = Counter(degrees)
+
+# Bar plot
+# fig, ax = plt.subplots(figsize=(18, 7))
+# ax.set_xlabel('Node degree')
+# ax.set_ylabel('Number of nodes')
+# plt.bar(numbers.keys(),
+#         numbers.values(),
+#         color='#0A047A')
+
+# plt.show()
+
+import torch.nn.functional as F
+from torch.nn import Linear, Dropout
+# from torch_geometric.nn import GCNConv, GATv2Conv
+
+class GAT(torch.nn.Module):
+  """Graph Attention Network"""
+  def __init__(self, dim_in, dim_h, dim_out, heads=8):
+    super().__init__()
+    self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
+    self.gat2 = GATv2Conv(dim_h*heads, dim_out, heads=1)
+    self.optimizer = torch.optim.Adam(self.parameters(),
+                                      lr=0.005,
+                                      weight_decay=5e-4)
+
+  def forward(self, x, edge_index):
+    h = self.gat1(x, edge_index)
+    h = F.elu(h)
+    h = F.dropout(h, p=0.6, training=self.training)
+    h = self.gat2(h, edge_index)
+    return h, F.log_softmax(h, dim=1)
+
+def accuracy(pred_y, y):
+    """Calculate accuracy."""
+    return ((pred_y == y).sum() / len(y)).item()
+
+def train(model, data):
+    """Train a GNN model and return the trained model."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = model.optimizer
+    epochs = 200
+
+    model.train()
+    for epoch in range(epochs+1):
+        # Training
+        optimizer.zero_grad()
+        _, out = model(data.x, data.edge_index)
+        loss = criterion(out[data.train_mask], data.y[data.train_mask])
+        acc = accuracy(out[data.train_mask].argmax(dim=1), data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+
+        # Validation
+        val_loss = criterion(out[data.val_mask], data.y[data.val_mask])
+        val_acc = accuracy(out[data.val_mask].argmax(dim=1), data.y[data.val_mask])
+
+        # Print metrics every 10 epochs
+        if(epoch % 10 == 0):
+            print(f'Epoch {epoch:>3} | Train Loss: {loss:.3f} | Train Acc: '
+                  f'{acc*100:>6.2f}% | Val Loss: {val_loss:.2f} | '
+                  f'Val Acc: {val_acc*100:.2f}%')
+
+    return model
+
+def test(model, data):
+    """Evaluate the model on test set and print the accuracy score."""
+    model.eval()
+    _, out = model(data.x, data.edge_index)
+    acc = accuracy(out.argmax(dim=1)[data.test_mask], data.y[data.test_mask])
+    return acc
+
+
+# %%time
+
+# Create GAT model
+gat = GAT(dataset.num_features, 8, dataset.num_classes)
+print(gat)
+
+# Train
+train(gat, data)
+
+# Test
+acc = test(gat, data)
+print(f'\nGAT test accuracy: {acc*100:.2f}%\n')
